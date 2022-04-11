1c734d586ec27c9d8e27c4b1137f76bd288f02c1
Jane <janechodance@gmail.com>
2022-04-11 12:11:30 -0400

    add reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
+import { formValues } from 'redux-form'
 import streams from '../apis/streams'
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types"
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAM, FETCH_STREAMS, DELETE_STREAM, EDIT_STREAM } from "./types"
 export const signIn = (userId) => {
     return {
         type: SIGN_IN,
@@ -18,4 +19,32 @@ export const createStream = (formValues) => {
      const response = await streams.post('/streams', formValues);
      dispatch({type: CREATE_STREAM, payload: response.data})
   }
+}
+
+export const fetchStreams = () => {
+    return async (dispatch) => {
+        const response = await streams.get('/streams');
+        dispatch({type: FETCH_STREAMS, payload: response.data})
+    }
+}
+
+export const fetchStream = (id) => {
+    return async (dispatch) =>{
+        const response = await streams.get(`/streams/${id}`);
+        dispatch({type: FETCH_STREAM, payload: response.data})
+    }
+}
+
+export const editStream = (id, formValues) => {
+     return async (dispatch) => {
+         const response = await streams.put(`/streams/${id}`, formValues);
+         dispatch({type: EDIT_STREAM, payload: response.data})
+     }
+}
+
+export const deleteStream = (id) => {
+    return async (dispatch) => {
+        await streams.delete(`/streams/${id}`);
+        dispatch({type: DELETE_STREAM, payload: id})
+    }
 }
