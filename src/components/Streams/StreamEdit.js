d05d9d7bf3c62bd96d99d5ca829bb843e361a3e4
Jane <janechodance@gmail.com>
2022-04-19 11:35:01 -0400

    refactor create stream with reuseable form

--- src/components/Streams/StreamEdit.js
+++ src/components/Streams/StreamEdit.js
@@ -1,14 +1,22 @@
 import React from "react";
 import {connect} from 'react-redux'
+import { fetchStream} from '../../actions'
 
-function StreamEdit(props){
-    console.log(props)
-    return(
+class StreamEdit extends React.Component{
+    componentDidMount(){
+       this.props.fetchStream(this.props.match.params.id)
+    }
+    
+    render(){
+        if (!this.props.stream){
+            return null
+        }
+        return <div>{this.props.stream.title}</div>
+    }
         
-        <div>StreamEdit</div>
-    )
+    
 };
 const mapStateToProps = (state, ownProps)=>{
     return{ stream: state.streams[ownProps.match.params.id] };
 }
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
