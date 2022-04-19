ee4e0168dc48f31cac111ed2663b3775388ba6ad
Jane <janechodance@gmail.com>
2022-04-19 11:03:49 -0400

    select stream to edit

--- src/components/Streams/StreamEdit.js
+++ src/components/Streams/StreamEdit.js
@@ -1,10 +1,14 @@
 import React from "react";
+import {connect} from 'react-redux'
 
-
-function StreamEdit(){
+function StreamEdit(props){
+    console.log(props)
     return(
+        
         <div>StreamEdit</div>
     )
 };
-
-export default StreamEdit;
+const mapStateToProps = (state, ownProps)=>{
+    return{ stream: state.streams[ownProps.match.params.id] };
+}
+export default connect(mapStateToProps)(StreamEdit);
