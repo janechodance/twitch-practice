41881823ccd0e1620434bbd4d5b7ee76f33cfa76
Jane <janechodance@gmail.com>
2022-04-18 21:20:30 -0400

    link to create page

--- src/components/Streams/StreamList.js
+++ src/components/Streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import { fetchStreams} from '../../actions'
 
 class StreamList extends React.Component{
@@ -23,17 +24,30 @@ class StreamList extends React.Component{
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera"/>
                     <div className="content">{stream.title}</div>
-                    <div className="description">{stream.desciption}</div>
-                    
+                    <div className="description">{stream.description}</div>
+                   
                 </div>
             ) 
         })
+    }
+
+    renderCreate(){
+       if (this.props.isSignedIn){
+           return (
+               <div style={{textAlign: 'right'}}>
+                   <Link to="/streams/new" className="ui button primary">
+               Create Stream</Link>
+               </div>
+               
+           )
+       }
     }
     render(){
         return(
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
             
         ) 
@@ -42,7 +56,9 @@ class StreamList extends React.Component{
 
 const mapStateToProps = (state)=>{
    return {streams: Object.values(state.streams),
-           currentUserId: state.auth.userId}
+           currentUserId: state.auth.userId,
+           isSignedIn: state.auth.isSignedIn
+        }
 }
 
 export default connect(mapStateToProps, {fetchStreams})(StreamList);
